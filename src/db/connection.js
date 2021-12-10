fecd121b0dd67702847ed5bbd1dbab39f18e9664
Ajinkya016 <77497555+Ajinkya016@users.noreply.github.com>
2021-12-10 15:37:43 +0530

    useUnifiedTopology

--- src/db/connection.js
+++ src/db/connection.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 mongoose.connect(`mongodb+srv://StarDucksAdmin:TheAjinkya111@users.yipik.mongodb.net/users?retryWrites=true&w=majority`, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    // useUnifiedTopology: true
+    useUnifiedTopology: true
 })
 .then(() => console.log('Database connected'))
 .catch(e => console.log(e));
